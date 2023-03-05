68378fd65f23df6adfc2db26a44174aacdef5f5b
Njiva Ariaina <njivaariaina47@gmail.com>
2023-03-05 05:26:19 -1200

    Construction d'une bouton retour vers la page d'accueil dans le navbar

--- navbar.py
+++ navbar.py
@@ -4,7 +4,9 @@ class Navbar:
     def get_navbar(self):
         result = '<html><head><title>Test Groupe 02</title>'
         result += '<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">'
+        result += '<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.2/css/all.min.css"/>'
         result += '</head>'
+        result += '<a href="/" data-toggle="tooltip" title="Accueil" class="btn btn-info"><i class="fa fa-home m-2"></i></a>'
         result += '<nav class="navbar navbar-expand navbar-light bg-info w-75 mt-4 mb-4 ml-auto mr-auto rounded" >'
         result += '<div class="container justify-content-md-center">'
         result += '<ul class="navbar-nav">'
